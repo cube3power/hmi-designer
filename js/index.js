778ec9bf6b8a142697f40d687f8f1ba353052814
Icer <liangshuangde@163.com>
2014-08-26 15:27:35 +0800

    initial check by clone from https://github.com/fatiherikli/mockup-designer.git

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//require('bower_components');
+// require.config({
+//     baseUrl: 'bower_components',
+//     paths: {
+//     }
+// });
+require('bower_components/jquery/dist/jquery.min');
+require('jquery-ui/jquery-ui.min');
+require('underscore/underscore');
+require('backbone/backbone-min');
+require('html2canvas/build/html2canvas.min');
+require('backbone.localStorage/backbone.localStorage-min');
+
